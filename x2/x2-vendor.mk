0e53f5c34dff760d3ec731a2268f9bb7988abee4
Juan Ramírez <juan1999l@gmail.com>
2019-08-24 03:12:57 +0200

    x2: add x10
     * x10 will have unified builds with x2
    
    LeEco Le Max 3
    ro.product.model=Letv X850
    ro.product.brand=LeEco
    ro.product.name=LeMax3
    ro.product.device=le_x10

--- x2/x2-vendor.mk
+++ x2/x2-vendor.mk
@@ -29,6 +29,9 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/x2/proprietary/vendor/etc/camera/imx230_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx230_chromatix.xml \
     vendor/leeco/x2/proprietary/vendor/etc/camera/imx230_lg_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx230_lg_chromatix.xml \
     vendor/leeco/x2/proprietary/vendor/etc/camera/imx230_sunny_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx230_sunny_chromatix.xml \
+    vendor/leeco/x2/proprietary/vendor/etc/camera/ov13870_front_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov13870_front_chromatix.xml \
+    vendor/leeco/x2/proprietary/vendor/etc/camera/ov13870_rear_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov13870_rear_chromatix.xml \
+    vendor/leeco/x2/proprietary/vendor/etc/camera/s5k3p3st_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3st_chromatix.xml \
     vendor/leeco/x2/proprietary/vendor/etc/camera/msm8996_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8996_camera.xml \
     vendor/leeco/x2/proprietary/vendor/etc/drc/drc_cfg_AZ.txt:$(TARGET_COPY_OUT_VENDOR)/etc/drc/drc_cfg_AZ.txt \
     vendor/leeco/x2/proprietary/vendor/etc/dts/dts-m6m8-lic.key:$(TARGET_COPY_OUT_VENDOR)/etc/dts/dts-m6m8-lic.key \
@@ -37,6 +40,9 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/x2/proprietary/vendor/firmware/a530_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.b02 \
     vendor/leeco/x2/proprietary/vendor/firmware/a530_zap.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.elf \
     vendor/leeco/x2/proprietary/vendor/firmware/a530_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.mdt \
+    vendor/leeco/x2/proprietary/vendor/firmware/le_x10/a530_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/le_x10/a530_zap.b01 \
+    vendor/leeco/x2/proprietary/vendor/firmware/le_x10/a530_zap.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/le_x10/a530_zap.elf \
+    vendor/leeco/x2/proprietary/vendor/firmware/le_x10/a530_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/le_x10/a530_zap.mdt \
     vendor/leeco/x2/proprietary/vendor/firmware/cppf.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/cppf.b00 \
     vendor/leeco/x2/proprietary/vendor/firmware/cppf.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/cppf.b01 \
     vendor/leeco/x2/proprietary/vendor/firmware/cppf.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/cppf.b02 \
@@ -127,6 +133,73 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov8865_ofilm_video_full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov8865_ofilm_video_full.so \
     vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov8865_ofilm_zsl_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov8865_ofilm_zsl_preview.so \
     vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov8865_ofilm_zsl_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov8865_ofilm_zsl_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_common.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_preview_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_preview_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_cpp_video_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_cpp_video_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_default_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_default_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_default_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_default_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_postproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_postproc.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_preview_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_preview_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_video_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_video_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_whitelist_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_whitelist_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_whitelist_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_whitelist_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_zsl_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_zsl_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_front_zsl_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_front_zsl_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_common.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_after_fusion_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_after_fusion_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_before_fusion_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_before_fusion_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_hfr_120.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_hfr_120.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_hfr_60.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_hfr_60.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_hybrid_fusion_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_hybrid_fusion_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_mfnr_fusion_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_mfnr_fusion_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_panorama.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_panorama.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_cpp_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_cpp_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_default_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_default_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_default_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_default_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_hfr_120.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_hfr_120.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_hfr_120_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_hfr_120_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_hfr_60.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_hfr_60.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_hfr_60_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_hfr_60_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_panorama.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_panorama.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_panorama_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_panorama_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_postproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_postproc.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_zsl_big_iris_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_zsl_big_iris_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_zsl_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_zsl_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_ov13870_rear_zsl_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_ov13870_rear_zsl_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_common.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_common.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_mfnr_fusion_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_mfnr_fusion_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_preview_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_preview_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_cpp_video_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_cpp_video_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_liveshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_postproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_postproc.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_preview.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_preview_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_preview_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_snapshot.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_video.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_video_whitelist.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_video_whitelist.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_whitelist_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_whitelist_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_whitelist_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_whitelist_video_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_zsl_preview_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_zsl_preview_a3.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libchromatix_s5k3p3st_zsl_video_a3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3st_zsl_video_a3.so \
     vendor/leeco/x2/proprietary/vendor/lib/libcppf.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcppf.so \
     vendor/leeco/x2/proprietary/vendor/lib/libflash_i2c.so:$(TARGET_COPY_OUT_VENDOR)/lib/libflash_i2c.so \
     vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_imx230.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_imx230.so \
@@ -134,6 +207,13 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_imx230_sunny.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_imx230_sunny.so \
     vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_sony_imx230_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_sony_imx230_eeprom.so \
     vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_sony_imx230_plus_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_sony_imx230_plus_eeprom.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_ov13870_cat24c64_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov13870_cat24c64_eeprom.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_ov13870_front.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov13870_front.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_ov13870_front_letv_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov13870_front_letv_eeprom.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_ov13870_letv_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov13870_letv_eeprom.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_ov13870_rear.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_ov13870_rear.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_s5k3p3st_sunny.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_s5k3p3st_sunny.so \
+    vendor/leeco/x2/proprietary/vendor/lib/libmmcamera_sunny_s5k3p3st_eeprom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmmcamera_sunny_s5k3p3st_eeprom.so \
     vendor/leeco/x2/proprietary/vendor/lib/libois_bu63165.so:$(TARGET_COPY_OUT_VENDOR)/lib/libois_bu63165.so \
     vendor/leeco/x2/proprietary/vendor/lib/libois_lc898122.so:$(TARGET_COPY_OUT_VENDOR)/lib/libois_lc898122.so \
     vendor/leeco/x2/proprietary/vendor/lib64/hw/fingerprint.qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.qcom.so \
